43f19b4f0414408b9c0e177c321f473776b20407
Emilia Afandieva <153540378+afandieva@users.noreply.github.com>
2024-12-20 16:42:40 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,11 +19,28 @@ int main() {
     g();
 }*/
 
-void sendIn(std::unique_ptr<Logger> x) {
+/*void sendIn(std::unique_ptr<Logger> x) {
 
 }
 int main() {
     auto smart = std::make_unique<Logger>();
     sendIn (std::move(smart));
 
+}*/
+
+int main() {
+    std::shared_ptr<int> ptr1=std::make_shared<int> (17);
+    std::cout << *ptr1 << "\n";
+    std::cout << ptr1.use_count()<<"\n";
+
+    auto ptr2=ptr1;
+    std::cout<<*ptr1<<"\n";
+    std::cout<<*ptr2<<"\n";
+    std::cout<<ptr1.use_count()<<"\n";
+    std::cout<<ptr2.use_count()<<"\n";
+    std::shared_ptr<int> ptr3;
+    std::cout<<ptr3.use_count()<<"\n";
+    ptr3=ptr1;
+    std::cout<<*ptr3<<"\n";
+    std::cout<<ptr3.use_count()<<"\n";
 }
